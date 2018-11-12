afdb961afd51fc01ebd668cbde5250c666f61eb8
pierrepoulouin <pierrepoulouin@gmail.com>
2018-11-12 16:38:06 +0100

    Debut du projet

---
+++ EsportDATA/src/main/java/co/simplon/esportdata/EsportDataApplication.java
@@ -0,0 +1,12 @@
+package co.simplon.esportdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsportDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsportDataApplication.class, args);
+	}
+}
