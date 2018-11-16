9507abe7b98ca8049e7a378324bfc9b3f7223932
pierrepoulouin <pierrepoulouin@gmail.com>
2018-11-16 10:42:32 +0100

    Creation de la superclass

---
+++ EsportDATA/src/main/java/co/simplon/esportdata/model/Personne.java
@@ -0,0 +1,33 @@
+package co.simplon.esportdata.model;
+
+public class Personne {
+
+	private String email;
+	private String mdp;
+	private String pseudo;
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getMdp() {
+		return mdp;
+	}
+
+	public void setMdp(String mdp) {
+		this.mdp = mdp;
+	}
+
+	public String getPseudo() {
+		return pseudo;
+	}
+
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+
+}
