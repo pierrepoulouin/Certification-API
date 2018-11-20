563421deef801179a45f0285c9d0cf37409792d4
pierrepoulouin <pierrepoulouin@gmail.com>
2018-11-20 14:37:57 +0100

    up du model

--- EsportDATA/src/main/java/co/simplon/esportdata/model/Utilisateur.java
+++ EsportDATA/src/main/java/co/simplon/esportdata/model/Utilisateur.java
@@ -1,18 +1,9 @@
 package co.simplon.esportdata.model;
 
-
-
-
-
-import javax.persistence.Basic;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-
-
 
 @Entity
 public class Utilisateur extends Personne {
@@ -21,24 +12,12 @@ public class Utilisateur extends Personne {
 	@GeneratedValue(strategy = GenerationType.AUTO) // Permet de faire l'auto_increment
 	private long userID;
 
-	@Basic
-	@Temporal(TemporalType.DATE)
-	private java.util.Date birthDate;
-
 	public long getUserID() {
 		return userID;
 	}
 
 	public void setUserID(long userID) {
 		this.userID = userID;
-	}
-
-	public java.util.Date getBirthDate() {
-		return birthDate;
-	}
-
-	public void setBirthDate(java.util.Date birthDate) {
-		this.birthDate = birthDate;
 	}
 
 }
