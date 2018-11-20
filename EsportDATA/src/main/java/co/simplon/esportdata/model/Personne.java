56219115626595c94864a66ad177bd79ccca1562
pierrepoulouin <pierrepoulouin@gmail.com>
2018-11-20 14:37:31 +0100

    Ajout d'attributs

--- EsportDATA/src/main/java/co/simplon/esportdata/model/Personne.java
+++ EsportDATA/src/main/java/co/simplon/esportdata/model/Personne.java
@@ -1,29 +1,37 @@
 package co.simplon.esportdata.model;
 
+import javax.persistence.Basic;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+
 
 @MappedSuperclass
 public class Personne {
 
-	private String email;
-	private String mdp;
+	@NotNull
+	@GeneratedValue(strategy = GenerationType.AUTO) // Permet de faire l'auto_increment
+	private long personneID; 
+	
 	private String pseudo;
+	private String firstName;
+	private String lastName;
+	private String address;
+	private String phone;
+	@Basic
+	@Temporal(TemporalType.DATE)
+	private java.util.Date birthDate;
 
-	public String getEmail() {
-		return email;
+	public long getPersonneID() {
+		return personneID;
 	}
 
-	public void setEmail(String email) {
-		this.email = email;
-	}
-
-	public String getMdp() {
-		return mdp;
-	}
-
-	public void setMdp(String mdp) {
-		this.mdp = mdp;
-	}
+	public void setPersonneID(long personneID) {
+		this.personneID = personneID;
+	} 
 
 	public String getPseudo() {
 		return pseudo;
@@ -31,6 +39,46 @@ public class Personne {
 
 	public void setPseudo(String pseudo) {
 		this.pseudo = pseudo;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstname) {
+		this.firstName = firstname;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public java.util.Date getBirthDate() {
+		return birthDate;
+	}
+
+	public void setBirthDate(java.util.Date birthDate) {
+		this.birthDate = birthDate;
 	}
 
 }
